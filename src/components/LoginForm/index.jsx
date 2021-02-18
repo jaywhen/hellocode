9f12d66732a9ae05364f4e7d2674c8df2f238a6e
jaywhen <jaywhen@qq.com>
2021-02-18 22:55:32 +0800

    fixed form push

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,26 +1,44 @@
 import React from "react";
-import { Card, Button, Form, Input } from "antd";
+import { Card, Button, Form, Input, Divider } from "antd";
 import { GithubOutlined, GoogleOutlined, WechatOutlined } from "@ant-design/icons";
 import "antd/dist/antd.css";
 import "./login-form.css";
 const LoginForm = () => {
+    const onFinish = (values) => {
+        console.log("suc ", values)
+        // 加密
+
+    }
     return(
-        // <Card className="innner-card" title="登录" style={{ textAlign: "center" }}>
         <Card className="inner-card"  cover={<span style={{ textAlign: "center", fontFamily: "sans-serif", fontSize: 18, marginTop: 20 }}>登录</span>}>
-            <Form>
-                <Form.Item>
+            <Form onFinish={onFinish}>
+                <Form.Item 
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: '请输入用户名！'
+                        }
+                    ]}>
                     <Input placeholder="手机号或邮箱" />
                 </Form.Item>
-                <Form.Item>
-                    <Input placeholder="密码" />
+                <Form.Item  
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: '请输入密码！'
+                        }
+                    ]}>
+                    <Input.Password placeholder="密码" />
                 </Form.Item>
                 <Form.Item>
-                    <Button className="login-form-button" type="primary">
+                    <Button className="login-form-button" type="primary" htmlType="submit" >
                         Login
                     </Button>
-                    Or <a href="">register now!</a>
+                    Or <a href="http://example.com/">register now!</a>
                 </Form.Item>
-                <hr />
+                <Divider />
                 <div className="social-login">
                     <span className="social-text">第三方账号登录</span>
                     <span className="social-button-group">
