6fcb43440dd6a73adfceb42d5ab80ec40bdee89a
jaywhen <jaywhen@qq.com>
2021-02-18 13:51:18 +0800

    fixed login layout

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -5,33 +5,32 @@ import "antd/dist/antd.css";
 import "./login-form.css";
 const LoginForm = () => {
     return(
-        <div className="login-form">
-            <Card className="inner-card"  cover={<span style={{ textAlign: "center", fontFamily: "sans-serif", fontSize: 26, marginTop: 10 }}>登录</span>}>
-                <Form>
-                    <Form.Item>
-                        <Input placeholder="手机号或邮箱" />
-                    </Form.Item>
-                    <Form.Item>
-                        <Input placeholder="密码" />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button className="login-form-button" type="primary">
-                            Login
-                        </Button>
-                        Or <a href="">register now!</a>
-                    </Form.Item>
-                    <hr />
-                    <div className="social-login">
-                        <span className="social-text">第三方账号登录</span>
-                        <span className="social-button-group">
-                            <Button shape="circle" type="text" icon={<GithubOutlined />}></Button>
-                            <Button shape="circle" type="text" icon={<WechatOutlined />}></Button>
-                            <Button shape="circle" type="text" icon={<GoogleOutlined />}></Button>
-                        </span>
-                    </div>
-                </Form>
-            </Card>
-        </div>
+        // <Card className="innner-card" title="登录" style={{ textAlign: "center" }}>
+        <Card className="inner-card"  cover={<span style={{ textAlign: "center", fontFamily: "sans-serif", fontSize: 18, marginTop: 20 }}>登录</span>}>
+            <Form>
+                <Form.Item>
+                    <Input placeholder="手机号或邮箱" />
+                </Form.Item>
+                <Form.Item>
+                    <Input placeholder="密码" />
+                </Form.Item>
+                <Form.Item>
+                    <Button className="login-form-button" type="primary">
+                        Login
+                    </Button>
+                    Or <a href="">register now!</a>
+                </Form.Item>
+                <hr />
+                <div className="social-login">
+                    <span className="social-text">第三方账号登录</span>
+                    <span className="social-button-group">
+                        <Button shape="circle" type="text" icon={<GithubOutlined />}></Button>
+                        <Button shape="circle" type="text" icon={<WechatOutlined />}></Button>
+                        <Button shape="circle" type="text" icon={<GoogleOutlined />}></Button>
+                    </span>
+                </div>
+            </Form>
+        </Card>
     );
 };
 
